f3a19970e12b8870c2a0c543369fc02064c694e6
danx <rushn24@gmail.com>
2023-04-17 18:41:54 -0400

    ran basic tests

--- calculator-test.js
+++ calculator-test.js
@@ -1,11 +1,13 @@
 
 it('should calculate the monthly rate correctly', function () {
-  // ...
+  const values = {amount: 100000, years: 10, rate: 12 };
+  expect(calculateMonthlyPayment(values)).toEqual('1434.71')
 });
 
 
 it("should return a result with 2 decimal places", function() {
-  // ..
+  const values = { amount:250000, years: 30, rate: 3.45};
+  expect(calculateMonthlyPayment(values)).toEqual('1115.65');
 });
 
 /// etc
